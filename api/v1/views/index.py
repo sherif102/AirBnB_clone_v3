a3a0e7417333dffc6817b569d251fd482b34340a
hyperbayse <hyperbayse@gmail.com>
2025-03-15 12:52:05 +0100

    stats created

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
 """index app"""
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    classes = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    records = {}
+    for key, value in classes.items():
+        records[key] = storage.count(value)
+    return jsonify(records)
