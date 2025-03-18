39936d506e256e84a567cb7828b67b95afb464f5
hyperbayse <hyperbayse@gmail.com>
2025-03-18 17:49:50 +0100

    update on CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 """python flask app"""
 
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views, jsonify
 from os import getenv
 
 
 app = Flask(__name__)
+cores = CORS(app_views, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
@@ -27,4 +29,4 @@ if __name__ == "__main__":
     app.run(
         host=getenv('HBNB_API_HOST', '0.0.0.0'),
         port=getenv('HBNB_API_PORT', '5000'),
-        threaded=True)
+        threaded=True, debug=True)
